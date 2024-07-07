97e944ec87d768da14a3c9c2fadfdab56d054a52
小新 <782446723@qq.com>
2024-07-07 22:59:02 +0800

    feat: 分界线

---
+++ src/components/DivideLine.vue
@@ -0,0 +1,18 @@
+<template>
+  <i class="divide-line"></i>
+</template>
+
+<script>
+export default {
+  name: "DivideLine",
+};
+</script>
+
+<style scoped>
+.divide-line {
+  display: block;
+  width: 100%;
+  height: 1px;
+  background-color: #e8e8e8;
+}
+</style>
