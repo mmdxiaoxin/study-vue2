aa5fc936eff467cf98684e58b1fc7e37f0531996
小新 <782446723@qq.com>
2024-07-07 22:58:19 +0800

    feat: vue2基本用法

---
+++ src/components/BasicComponents.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <h1>基本用法测试</h1>
+    <p>{{ msg }}</p>
+    <input v-model="msg" />
+    <button @click="reverseString">反转字符串</button>
+    <p>{{ firstName }} + {{ lastName }} => {{ fullName }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BasicComponents",
+  components: {},
+  data() {
+    return {
+      msg: "Hello Vue!",
+      firstName: "John",
+      lastName: "Doe",
+    };
+  },
+  computed: {
+    fullName: {
+      // Getter
+      get() {
+        return this.firstName + " " + this.lastName;
+      },
+      // Setter
+      set(newValue) {
+        const names = newValue.split(" ");
+        this.firstName = names[0];
+        this.lastName = names[names.length - 1];
+      },
+    },
+  },
+  methods: {
+    reverseString: function () {
+      this.msg = this.msg.split("").reverse().join("");
+    },
+  },
+  watch: {
+    // 监听msg变化
+    msg: function (newVal, oldVal) {
+      console.log("msg changed from " + oldVal + " to " + newVal);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
