655ed796fcf179b249b6e93ebd7e2a0753649cae
小新 <782446723@qq.com>
2024-07-07 22:58:49 +0800

    feat: 样式绑定

---
+++ src/components/StyleBind.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>样式绑定</h1>
+    <p :style="{ color: color, fontSize: fontSize }">这是一段红色的文字</p>
+    <p :style="styleObject">这是一段蓝色的文字</p>
+    <p :class="{ active: isActive }">这是一段带有active类的文字</p>
+    <input type="button" value="Toggle Active" @click="toggleActive" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StyleBind",
+  data() {
+    return {
+      color: "red",
+      fontSize: "30px",
+      isActive: true,
+      styleObject: {
+        color: "blue",
+        fontSize: "40px",
+      },
+    };
+  },
+  methods: {
+    toggleActive() {
+      this.isActive = !this.isActive;
+    },
+  },
+};
+</script>
+
+<style scoped></style>
